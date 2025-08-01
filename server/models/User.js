12f4fcacce377fa6b81c04ccea8e96063e4455e7
Sahil9309 <sahiltalwekar123@gmail.com>
2025-08-01 17:44:13 +0530

    updated user schema

--- server/models/User.js
+++ server/models/User.js
@@ -12,60 +12,14 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: function() {
-      return this.authProvider === 'local';
-    }
+    required: true
   },
-  googleId: {
-    type: String,
-    unique: true,
-    sparse: true // Allows multiple null values but ensures unique non-null values
-  },
-  profilePicture: {
-    type: String,
-    default: null
-  },
-  authProvider: {
-    type: String,
-    enum: ['local', 'google'],
-    default: 'local'
-  },
-  isEmailVerified: {
+  isAdmin: {
     type: Boolean,
     default: false
   }
 }, {
   timestamps: true
-});
-
-// Virtual for full name if needed
-userSchema.virtual('fullName').get(function() {
-  return this.name;
-});
-
-// Method to check if user can login with password
-userSchema.methods.canLoginWithPassword = function() {
-  return this.authProvider === 'local' && this.password;
-};
-
-// Method to check if user is OAuth user
-userSchema.methods.isOAuthUser = function() {
-  return this.authProvider !== 'local';
-};
-
-// Pre-save middleware to ensure data consistency
-userSchema.pre('save', function(next) {
-  // If this is a Google user, ensure googleId is present
-  if (this.authProvider === 'google' && !this.googleId) {
-    return next(new Error('Google ID is required for Google authentication'));
-  }
-  
-  // If this is a local user, ensure password is present
-  if (this.authProvider === 'local' && !this.password) {
-    return next(new Error('Password is required for local authentication'));
-  }
-  
-  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
